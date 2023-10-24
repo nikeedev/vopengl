35abd10e08290fd6158a6c72b4cb2f0b40bab46d
nikeedev <nikita.gon.dev@gmail.com>
2023-10-24 09:53:41 +0200

    first commit

---
+++ src/main.v
@@ -0,0 +1,10 @@
+module main
+#flag -Ivendor/include
+#flag -Lvendor/lib/libglfw3.a
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+
+fn main() {
+	println('Hello World!')
+}
---
+++ v.mod
@@ -0,0 +1,7 @@
+Module {
+	name: 'vopengl'
+	description: 'OpenGL project using C header in V'
+	version: '0.1.0'
+	license: 'MTI'
+	dependencies: []
+}
